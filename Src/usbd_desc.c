b31eb76566a55f0331c1231fe32e9a73fe02e8a8
褚仕成 <1468559561@qq.com>
2023-02-25 22:58:16 +0800

    修改USB PID便于辨识

--- Src/usbd_desc.c
+++ Src/usbd_desc.c
@@ -65,7 +65,7 @@
 #define USBD_VID     1155
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "STMicroelectronics"
-#define USBD_PID_FS     22336
+#define USBD_PID_FS     22335
 #define USBD_PRODUCT_STRING_FS     "STM32 Virtual ComPort"
 #define USBD_CONFIGURATION_STRING_FS     "CDC Config"
 #define USBD_INTERFACE_STRING_FS     "CDC Interface"
